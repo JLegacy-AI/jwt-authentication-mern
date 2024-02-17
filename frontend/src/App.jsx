1ea6a7fbfeff9d5b3756372e27845ea0c80d392c
jlegacy-ai <jamalmuhammad.bsaif20@iba-suk.edu.pk>
2024-02-17 05:31:11 +0500

    Sign Up & In pages add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/SignUp";
+import Dashboard from "./Pages/Dashboard";
+
 function App() {
-  return <>Hello World</>;
+  return (
+    <>
+      <Routes>
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/*" element={<SignIn />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
